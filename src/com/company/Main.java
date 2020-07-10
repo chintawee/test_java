63e58acf2a1b0d552f5c0a0e82bd874354ca4aee
chintawee <59070023@it.kmitl.ac.th>
2020-07-10 15:58:53 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner myPoon = new Scanner(System.in);
+
+	String userName =  myPoon.nextLine();
+	System.out.println("Username is: " + userName);
+    }
+}
